e3319cee5b4a531fb0a039a6a4ea54bd30582826
mati-caliz <matiascaliz@hotmail.com>
2024-11-29 17:33:56 -0300

    fix

--- frontend/src/redux/redux/apiConstants/ApiConstants.js
+++ frontend/src/redux/redux/apiConstants/ApiConstants.js
@@ -1,7 +1,5 @@
 const ApiConstants = {
-    baseUrl: process.env.NODE_ENV === 'production'
-        ? 'TODO AWS url'
-        : 'http://localhost:8000/',
+    baseUrl: process.env.REACT_APP_API_URL || 'http://localhost:8000/',
     analysisUrls: {
         audio: 'analyses/audio',
         getPreviousAnalyses: 'analyses/latest',
